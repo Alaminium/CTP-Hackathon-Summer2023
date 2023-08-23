f3179b0039cda16ea291836d3222f80acd66b380
Alaminium <alaminmiah.codes@gmail.com>
2023-08-23 17:35:06 -0400

    Made some changes to the html file to fit certain types of monitors. Added the start of possible functions to use for JS. Briefly started style sheet.

---
+++ main.js
@@ -0,0 +1,15 @@
+function createSquare(length, width){
+
+}
+
+function createRectangle(length,width){
+
+}
+
+function createTrinagle(length,width){
+
+}
+
+function createCirlce(length,width){
+    
+}
