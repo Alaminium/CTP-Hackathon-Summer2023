77ca84a0a13293845792ebd509e7c2e97324b9eb
Alaminium <alaminmiah.codes@gmail.com>
2023-08-24 12:31:19 -0400

    made some changes to js file, edited shapes

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-function createSquare(length, width){
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 
+function createRectangle(backgroundColor, shapelineColor, shapefillingColor, length, width){
+    context.fillStyle(backgroundColor);
+    context.fillRect(0,0,length,width);
+    context.strokeStyle(shapelineColor);
+    context.fillStyle(shapefillingColor);
+    context.fillRect(length/2,width/2,length/3,width/4);
 }
 
-function createRectangle(length,width){
-
+function createTrinagle(backgroundColor, shapelineColor, shapefillingColor, length, width){
+    context.fillStyle(backgroundColor);
+    context.fillRect(0,0,length,width);
+    context.beginPath();
+    context.strokeStyle(shapelineColor);
+    context.fillStyle(shapefillingColor);
+    context.moveTo();
+    context.lineTo();
+    context.lineTo();
+    context.fill();
 }
 
-function createTrinagle(length,width){
-
-}
-
-function createCirlce(length,width){
-    
+function createCirlce(backgroundColor, shapelineColor, shapefillingColor, length, width){
+    context.fillStyle(backgroundColor);
+    context.fillRect(0,0,length,width);
+    context.beginPath();
+    context.strokeStyle(shapelineColor);
+    context.fillStyle(shapefillingColor);
+    context.fill();
+    context.arc(length/2,width/2,width/2,0,2*Math.PI);
+    context.stroke();
 }
